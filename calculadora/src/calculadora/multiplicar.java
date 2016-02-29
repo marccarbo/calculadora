a56d29d60d89cb26ae56db1683d9cd92458da78b
Zagren <kurogane.akumu@hotmail.es>
2016-02-29 21:16:37 +0100

    multiplicacio

---
+++ calculadora/src/calculadora/multiplicar.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class multiplicar {
+	public class multi {
+		private int num1;
+		private int num2;
+		public void Suma(int numero1, int numero2){
+			num1=numero1;
+			num2=numero2;
+		}
+		public int multiplicar(){
+			int resultado=num1*num2;
+			return resultado;
+		}
+	}
+}
