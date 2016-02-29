50c0f5f56bc2e75b9cd89be7d250a09337a4258a
Zagren <kurogane.akumu@hotmail.es>
2016-02-29 20:25:28 +0100

    Principal

---
+++ calculadora/src/calculadora/suma.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+public class suma {
+	private int num1;
+	private int num2;
+	public void Suma(int numero1, int numero2){
+		num1=numero1;
+		num2=numero2;
+	}
+	public int sumar(){
+		int resultado=num1+num2;
+		return resultado;
+	}
+}
